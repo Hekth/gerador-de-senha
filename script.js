021d74c659cc7342198e519157f1fa361d73b0c2
Heitor da Costa Cândido <151862970+Hekth@users.noreply.github.com>
2024-01-01 19:57:24 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,19 +4,85 @@ const gerarSenhaBtn = document.querySelector('.gerar-senha-btn');
 const senhaContainer = document.querySelector('.senha-container');
 const senha = document.querySelector('.senha-container__senha');
 const senhaMsg = document.querySelector('.senha-container__msg');
-const inputNumeros = document.querySelector('#numeros');
-const inputEspeciais = document.querySelector('#caracteres-especiais');
+const btnConfig = document.querySelector('.btn-config');
+const configuracoes = document.querySelector('.configuracoes');
+const configBtns = document.querySelectorAll('.configuracoes__btn-config');
+const configCampos = document.querySelectorAll('.configuracoes__container input');
+const caracteresRestantes = document.querySelector('.caracteres-restantes');
+const palavraEspecifica = document.querySelector('#palavra-especifica');
 
-let caracteres = 'abcdefghijklmnopqrstuvxywzABCDEFGHIJKLMNOPQRSTUVXYWZ0123456789!@#$%&*.?-_;';
+// const inputNumeros = document.querySelector('#numeros');
+let estado = {
+    palavra: '',
+    numeros: true,
+    letras: true,
+    especiais: true
+};
+
+const caracteres = {
+    letras: ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','X','W','Y','Z',
+            'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','w','y','z'],
+    numeros: ['0','1','2','3','4','5','6','7','8','9'],
+    especiais: ['!','@','#','$','%','&','*','_','-','.',';','?']
+};
+
+palavraEspecifica.addEventListener('input', (e) => {
+    const diferenca = (inputRange.value - 1) - e.target.value.length;
+    if (diferenca >= 0) {
+        caracteresRestantes.innerText = diferenca;
+    }
+})
+
+/* funções do dialog */
+btnConfig.addEventListener('click', () => configuracoes.showModal());
+
+configBtns[0].addEventListener('click', () => {
+    /* adicionando os valores do formulario ao estado */
+    configCampos.forEach((campo) => {
+        const valor = campo.hasAttribute('checked') ? campo.checked : campo.value;
+        estado[campo.name] = valor;
+    });
+
+    if (validaCampos()) {
+        configuracoes.close();
+    }
+    
+});
+
+configBtns[1].addEventListener('click', () => {
+    configuracoes.close();
+    estado = {
+        palavra: '',
+        numeros: true,
+        letras: true,
+        especiais: true
+    }
+});
 
 /* mostrando o valor do input range na tela */
+inputRange.addEventListener('change', () => {
+    estado = {
+        palavra: '',
+        numeros: true,
+        letras: true,
+        especiais: true
+    };
+
+    configCampos.forEach((campo) => {
+        campo.hasAttribute('checked') ? campo.checked = true : campo.value = '';
+    });
+});
+
 inputRange.addEventListener('input', ({ target }) => {
-    spanQtdCaracteres.innerText = target.value
+    spanQtdCaracteres.innerText = target.value;
+    caracteresRestantes.innerText = target.value - 1;
+    palavraEspecifica.setAttribute('maxlength', target.value - 1);
 });
 
 gerarSenhaBtn.addEventListener('click', () => {
     senha.innerText = '';
     filtraSenhaAleatoria();
+    mostraSenha();
     senhaMsg.innerHTML = `Clique na senha acima para copiá-la!&#x261D;`;
 });
 
@@ -27,41 +93,45 @@ senha.addEventListener('click', () => {
 
 /* gerando uma senha aleatoria com base no valor do input range */
 function gerarSenhaAleatoria(caracteresFiltrados) {
+    let senhaAleatoria = '';
     const tamanhoCaracteres = caracteresFiltrados.length;
     const qtdCaracteres = Number(inputRange.value);
-    for (let i = 0; i < qtdCaracteres; i++) {
+    for (let i = 0; i < qtdCaracteres - estado.palavra.length; i++) {
         const numeroAleatorio = Math.floor(Math.random() * tamanhoCaracteres);
-        senha.innerText += caracteresFiltrados[numeroAleatorio]
+        senhaAleatoria += caracteresFiltrados[numeroAleatorio]
     }
-}
+
+    if (estado.palavra) {
+        const numeroAleatorio = Math.floor(Math.random() * (senhaAleatoria.length + 1));
+        const senhaAleatoriaArray = senhaAleatoria.split('');
+        senhaAleatoriaArray.splice(numeroAleatorio, 0, estado.palavra)
+        senhaAleatoria = senhaAleatoriaArray.join('');
+    }
+
+    senha.innerText = senhaAleatoria;
+}   
 
 function mostraSenha() {
     senhaContainer.style.display = 'block';
 }
 
 function filtraSenhaAleatoria() {
-    if (inputNumeros.checked && inputEspeciais.checked) {
-        gerarSenhaAleatoria(caracteres);
-        mostraSenha();
-        return;
-    }
+    let todosCaracteres = [];
+    const filtros = Object.entries(estado).filter((e) => e[1]).map((element) => element[0]);
+    filtros.forEach((e) => {
+        if (e !== 'palavra') todosCaracteres = todosCaracteres.concat(caracteres[e]);
+    });
+    gerarSenhaAleatoria(todosCaracteres);
+}
 
-    if (inputNumeros.checked) {
-        const filtraCaracteres = caracteres.replace(/[!@#$%&*.?_;-]/g, '');
-        gerarSenhaAleatoria(filtraCaracteres);
-        mostraSenha();
-        return;
+function validaCampos() {
+    try {
+        const valoresDoEstado = Object.values(estado);
+        valoresDoEstado.shift();
+        const validacao = valoresDoEstado.every((valor) => !valor);
+        if (validacao) throw new Error('É preciso selecionar pelo menos UMA das opções.');
+        return true;
+    } catch (error) {
+        return alert(error);
     }
-
-    if (inputEspeciais.checked) {
-        const filtraCaracteres = caracteres.replace(/[0-9]/g, '');
-        gerarSenhaAleatoria(filtraCaracteres);
-        mostraSenha();
-        return;
-    }
-    
-    const filtraCaracteres = caracteres.replace(/[-!@#$%&*.?_;0-9]/g, '');
-    gerarSenhaAleatoria(filtraCaracteres);
-    mostraSenha();
-    return;
 }
